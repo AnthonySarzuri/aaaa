14e075aa138bb03166826d5247925b48d0c9c94f
Anthony <ssa2023593@est.univalle.edu>
2022-08-24 16:36:04 -0400

    es trabajo de aula aprendiendo kotlin, un mundo nuevo :)

--- src/main/kotlin/set.kt
+++ src/main/kotlin/set.kt
@@ -1,3 +1,25 @@
 fun main() {
-    println()
+    val numeros= listOf(4,3,2,1,5,7)
+    val impares=numeros.filter {
+        numero-> numero%2!=0 //Definicion exacta de lo que es una funcion flecha
+    }
+    //pero en resumen es...
+    val impar=numeros.filter { it % 2!=0 }
+
+    println(impares)
+    println(impar)
+
+    val resultado=numeros.map{valor->(2*valor)-1}
+
+    println(resultado)
+
+    //reduce barre tu lista de izq a der
+
+
+
+
+
+    numeros.reversed().reduce{total,valor->total*valor}
+
 }
+
